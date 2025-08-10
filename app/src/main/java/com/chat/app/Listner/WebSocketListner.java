476f325080a3a8e13ffbd2f340100e3ad43b754d
anishraj-38 <anishkumar200507@gmail.com>
2025-08-10 11:08:59 +0530

    git commit -m main

--- app/src/main/java/com/chat/app/Listner/WebSocketListner.java
+++ app/src/main/java/com/chat/app/Listner/WebSocketListner.java
@@ -1,4 +1,34 @@
 package com.chat.app.Listner;
 
+import com.chat.app.Service.UserService;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.util.logging.Logger;
+
+import static com.chat.app.Service.UserService.*;
+
+@Component
 public class WebSocketListner {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(WebSocketListner.class);
+
+    @EventListener
+    public void handleWebsocketConnectListener(SessionConnectedEvent event){
+        logger.info(" Connected to websocket ");
+    }
+    public void handleWebsocketDisconnectListener(SessionDisconnectEvent event){
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = headerAccessor.getSessionAttributes().get("username").toString();
+
+       userService.setUserOnlineStatus(username,false);
+    }
 }
